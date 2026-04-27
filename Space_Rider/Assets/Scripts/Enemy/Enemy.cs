e63be1499953cae658d14e49dea2c3eff4e7173f
Kushi <68166561+kushiros@users.noreply.github.com>
2026-04-27 12:55:19 -0300

    Added turret mechanic and enemies
    +added basic turret mechanic (life,range,firerate and upgrade)
    +added basic enemy logic (total life, reward,mini blink when get damage)

--- Space_Rider/Assets/Scripts/Enemy/Enemy.cs
+++ Space_Rider/Assets/Scripts/Enemy/Enemy.cs
@@ -1,33 +1,51 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int health = 100;
     [SerializeField] private int reward = 50;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Color damageColor = Color.red;
+    [SerializeField] private float flashDuration = 0.1f;
+
+    private Color originalColor;
+
     void Start()
     {
-        
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+        originalColor = spriteRenderer.color;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void TakeDamage(int damage)
     {
         health -= damage;
+        FlashDamage();
+
         if (health <= 0)
         {
             Die();
         }
     }
+
+    private void FlashDamage()
+    {
+        spriteRenderer.color = damageColor;
+        Invoke(nameof(ResetColor), flashDuration);
+    }
+
+    private void ResetColor()
+    {
+        spriteRenderer.color = originalColor;
+    }
+
     private void Die()
     {
         CurrencyEvents.OnEnemyKilled?.Invoke(this);
         Destroy(gameObject);
     }
+
     public int GetReward()
     {
         return reward;
