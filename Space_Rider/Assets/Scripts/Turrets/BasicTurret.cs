6b264a38f3d51e6e4e2955881ce8ae96498d9e6a
urrutiamateo <urrutiamateo7@gmail.com>
2026-05-11 06:25:20 -0300

    create turret prefabs
    Added BasicTurret prefab
    Added DoubleTurret prefab
    Added LaserTurret prefab
    Added fire points for shooting setup

--- Space_Rider/Assets/Scripts/Turrets/BasicTurret.cs
+++ Space_Rider/Assets/Scripts/Turrets/BasicTurret.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class BasicTurret : Turret
 {
-
+    [SerializeField] protected Transform firePoint;
 }
--- Space_Rider/Assets/Scripts/Turrets/DoubleTurret.cs
+++ Space_Rider/Assets/Scripts/Turrets/DoubleTurret.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class BobleTurret: Turret
 {
+    
+    [SerializeField] private Transform leftFirePoint;
+    [SerializeField] private Transform rightFirePoint;
+
     protected override void Shoot(Enemy enemy)
     {
         Debug.Log("Double shoot");
